6f8ebe97edce56970430771bcde9ba10f2e7390d
Dave Nielsen <dhniels@gmail.com>
2015-03-25 17:19:23 -0600

    API fixes
    finally got the API working. form now captures search terms with JS,
    then sends them to the API where results are now being returned to the
    console. next we need to parse and store the needed results and display
    them on the page.

--- js/search.js
+++ js/search.js
@@ -1,8 +1,9 @@
 function search(e) {
+	e.preventDefault();
 	var searchTerm = document.getElementById('searchTerm').value;
 	searchTerm = searchTerm.split(' ').join('+');
 	var script = document.createElement('script');
-	script.src = 'https://itunes.apple.com/search?term='+searchTerm+'&entity=software&callback=itunesResponse';
+	script.src = 'https://itunes.apple.com/search?term='+searchTerm+'&entity=software&callback=handleApiResponse';
 	document.getElementsByTagName('head')[0].appendChild(script);
 }
 
@@ -10,6 +11,6 @@ function handleApiResponse(data){
     console.log(data.results);
 }
 
-document.getElementById('form').addEventListener(
-    'submit', search, false;
+document.getElementById('searchBar').addEventListener(
+    'submit', search, false
 );
