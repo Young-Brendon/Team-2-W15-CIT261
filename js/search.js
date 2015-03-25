00c9f33976db15cc40904600289836d91bfc6dd3
Dave Nielsen <dhniels@gmail.com>
2015-03-25 17:01:00 -0600

    fixes for JS
    added and fixed so that we can now connect to the iTunes api and pass
    it the search terms

--- js/search.js
+++ js/search.js
@@ -1,3 +1,15 @@
-function search() {
+function search(e) {
 	var searchTerm = document.getElementById('searchTerm').value;
+	searchTerm = searchTerm.split(' ').join('+');
+	var script = document.createElement('script');
+	script.src = 'https://itunes.apple.com/search?term='+searchTerm+'&entity=software&callback=itunesResponse';
+	document.getElementsByTagName('head')[0].appendChild(script);
 }
+
+function handleApiResponse(data){
+    console.log(data.results);
+}
+
+document.getElementById('form').addEventListener(
+    'submit', search, false;
+);
