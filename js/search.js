a1a9aa15716a338bb45bc7dfea5650c035d500b8
Dave Nielsen <dhniels@gmail.com>
2015-04-01 16:20:06 -0600

    app now displays app name and app icon in results
    need to figure out how to display the items in the following structure
    in the HTML
    
    EXAMPLE HTML STRUCTURE FOR EACH APP IN SEARCH RESULTS:
    <ul>
        <li>
            <h3>Yahoo News</h3>
            <img src="#">
            <ul>
                <li>Developer name</li>
                <li>URL</li>
            </ul>
        </li>
        <li>
            <h3>Yahoo Weather</h3>
            <img src="#">
            <ul>
                <li>Deverloper</li>
                <li>URL</li>
            </ul>
        </li>
    </ul>

--- js/search.js
+++ js/search.js
@@ -3,12 +3,31 @@ function search(e) {
 	var searchTerm = document.getElementById('searchTerm').value;
 	searchTerm = searchTerm.split(' ').join('+');
 	var script = document.createElement('script');
-	script.src = 'https://itunes.apple.com/search?term='+searchTerm+'&entity=software&callback=handleApiResponse';
+	script.src = 'https://itunes.apple.com/search?term='+searchTerm+'&entity=software&callback=handleApiResponse&limit=30';
 	document.getElementsByTagName('head')[0].appendChild(script);
 }
 
 function handleApiResponse(data){
     console.log(data.results);
+
+    // Get ul#list
+    var list = document.getElementById("list");
+
+    // Store data and display on the page
+    var frag = document.createDocumentFragment();
+    for (i = 0; i < data.results.length; i++) {
+    	// APP NAME
+    	var appName = document.createElement("li");
+    	appName.innerHTML = "<h3>" + data.results[i].trackName + "</h3>";
+    	frag.appendChild(appName);
+
+    	// APP ICON
+    	var appIcon = document.createElement("img");
+    	appIcon.setAttribute('src', data.results[i].artworkUrl60);
+    	frag.appendChild(appIcon);
+    };
+    list.appendChild(frag);
+    
 }
 
 document.getElementById('searchBar').addEventListener(
